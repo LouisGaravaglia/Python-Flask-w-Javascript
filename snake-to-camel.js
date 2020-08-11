305ac9e07eb047cefd3857ed35e1a1e274aefa2f
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-08-11 13:19:20 -0700

    finsihed camelCase test

--- snake-to-camel.js
+++ snake-to-camel.js
@@ -1,2 +1,26 @@
-function snakeToCamel() {}
+function snakeToCamel(arg) {
+
+split = arg.split(/_/g)
+
+for (let i = 1; i < split.length; i++) {
+
+secondHalf = split[i].substring(1)
+
+    if (secondHalf == secondHalf.toUpperCase()) {
+        split[i] = split[i].substring(0,1).toUpperCase() + secondHalf
+    } else {
+        split[i] = split[i].substring(0,1).toUpperCase() + split[i].substring(1).toLowerCase()
+    }
+
+}
+
+camel = split.join("")
+
+return camel;
+}
+
+
+
+
+
 
