3f88a7124ac8145e581ccbc3cfa8ad40e0be89ea
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-08-11 14:30:18 -0700

    finished star grid problem

--- star-out-grid.js
+++ star-out-grid.js
@@ -1,2 +1,28 @@
 function starOutGrid(grid) {
+
+    xCoordinates = []
+    yCoordinates = []
+
+   for (let x = 0; x < grid.length; x++) {
+       for (let y = 0; y < grid[x].length; y++) {
+           if (grid[x][y] == "*") {
+               yCoordinates.push(y)
+               xCoordinates.push(x)
+           }
+        }   
+   } 
+
+    for (let y = 0; y < yCoordinates.length; y++) {
+        for (let x = 0; x < grid.length; x++) {
+               grid[x][yCoordinates[y]] = "*"
+            }
+        }   
+
+    for (let x = 0; x < xCoordinates.length; x++) {
+        grid[xCoordinates[x]] = ["*", "*", "*"]
+        }   
+    
+return grid;
+
 }
+
